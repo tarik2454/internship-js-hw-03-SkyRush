76eab65bc156b2156465da1baff50dbfa2c7f280
Taras Kovalyk <tarik2454@gmail.com>
2025-12-18 13:57:19 +0200

    style: enhance GameGrid component with flexbox layout, add success and fail cell styles, and integrate icons for gem and mine statuses

--- src/modules/mines/components/GameGrid.tsx
+++ src/modules/mines/components/GameGrid.tsx
@@ -1,17 +1,32 @@
 import { cx } from "../../../utils/classNames";
+import { BombIcon } from "../../../shared/icons/bomb";
+import { DiamondIcon } from "../../../shared/icons/diamond";
 import styles from "./GameGrid.module.scss";
+
+type CellStatus = "hidden" | "gem" | "mine";
 
 interface GameGridProps {
   className?: string;
 }
 
 export const GameGrid = ({ className }: GameGridProps) => {
-  const cells = Array.from({ length: 25 }, (_, i) => i);
+  const cells: CellStatus[] = Array(25).fill("hidden");
+  cells[2] = "mine";
+  cells[4] = "gem";
 
   return (
     <div className={cx(styles.grid, className)}>
-      {cells.map((i) => (
-        <div key={i} className={styles.cell} />
+      {cells.map((status, i) => (
+        <div
+          key={i}
+          className={cx(styles.cell, {
+            [styles.cellSuccess]: status === "gem",
+            [styles.cellFail]: status === "mine",
+          })}
+        >
+          {status === "gem" && <DiamondIcon />}
+          {status === "mine" && <BombIcon />}
+        </div>
       ))}
     </div>
   );
---
+++ src/shared/icons/diamond.tsx
@@ -0,0 +1,26 @@
+export const DiamondIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#fff"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M10.5 3 8 9l4 13 4-13-2.5-6"
+      />
+      <path
+        stroke="#fff"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M17 3a2 2 0 0 1 1.6.8l3 4a2 2 0 0 1 .013 2.382l-7.99 10.986a2 2 0 0 1-3.247 0l-7.99-10.986A2 2 0 0 1 2.4 7.8l2.998-3.997A2 2 0 0 1 7 3zM2 9h20"
+      />
+    </svg>
+  );
+};
